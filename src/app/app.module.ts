252453befb40ededeb20da2c8c70591193ab4cf4
ClaydLena <92931103+ClaydLena@users.noreply.github.com>
2022-09-17 22:54:40 +0200

    Criacao de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { InputComponent } from './components/input/input.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    InputComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
